db1c03b2622a3a11577e263cae0c147bb3f16ab7
Rui Costa <rui.pfcosta@gmail.com>
2015-07-21 00:11:43 +0100

    Reorganised the project and integrated Stack.

---
+++ test/Spec.hs
@@ -0,0 +1,10 @@
+import Ipseity.Ipseity
+
+main :: IO ()
+main = ipseity "test.ini"
+
+--wrong    = ipseity "wrong.ini"
+--wrong2   = ipseity "wrong2.ini"
+--wrong3   = ipseity "wrong3.ini"
+--wrong4   = ipseity "wrong4.ini"
+--notfound = ipseity "nofile.ini"
